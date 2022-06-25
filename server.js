a52831c5ac82ffeaebd901fb255695b1afdf1e9c
Andrey G <a.gavrilov@promo-bot.ru>
2022-06-25 15:38:11 +0500

    ejs

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const path = require('path')
 
-const createPath = (page) => path.resolve(__dirname, '8_views', `${page}.html`)
+const createPath = (page) =>
+  path.resolve(__dirname, 'ejs8_views', `${page}.ejs`)
 
 const app = express()
+
+app.set('view engine', 'ejs')
 
 const PORT = 3000
 
@@ -13,18 +16,50 @@ app.listen(PORT, (error) => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'))
+  const title = 'Home'
+  res.render(createPath('index'), { title })
 })
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'))
+  const title = 'Contacts'
+  const contacts = [
+    {
+      name: 'YouTube',
+      link: 'http://youtube.com',
+    },
+    {
+      name: 'GitHub',
+      link: 'http://github.com',
+    },
+    {
+      name: 'Twitter',
+      link: 'http://twitter.com',
+    },
+  ]
+  res.render(createPath('contacts'), { contacts, title })
 })
 
-app.get('/about-us', (req, res) => {
-  res.redirect('/contacts')
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/contacts')
+// })
+
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post'
+  res.render(createPath('post'), { title })
+})
+
+app.get('/posts', (req, res) => {
+  const title = 'Posts'
+  res.render(createPath('posts'), { title })
+})
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add-post'
+  res.render(createPath('add-post'), { title })
 })
 
 app.use((req, res) => {
+  const title = 'Error'
   res.statusCode = 404
-  res.sendFile(createPath('error'))
+  res.render(createPath('error'), { title })
 })
