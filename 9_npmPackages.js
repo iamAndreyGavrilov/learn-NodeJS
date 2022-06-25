84a4fde6e1b3609ec53f92bdd35acb470bc20ec9
Andrey G <a.gavrilov@promo-bot.ru>
2022-06-25 13:04:33 +0500

    установка npm

---
+++ 9_npmPackages.js
@@ -0,0 +1,61 @@
+const http = require('http')
+
+const fs = require('fs')
+
+const path = require('path')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+  console.log('Server request')
+  console.log('Just for test')
+
+  res.setHeader('Content-Type', 'text/html')
+
+  const createPath = (page) =>
+    path.resolve(__dirname, '8_views', `${page}.html`)
+
+  let basePath = ''
+
+  switch (req.url) {
+    case '/':
+    case '/home':
+    case '/index.html':
+      basePath = createPath('index')
+      res.statusCode = 200
+      break
+
+    case '/about-us':
+      res.statusCode = 301
+      res.setHeader('Location', '/contacts')
+      res.end()
+      break
+
+    case '/contacts':
+      basePath = createPath('contacts')
+      res.statusCode = 200
+      break
+    default:
+      basePath = createPath('error')
+      res.statusCode = 404
+      break
+  }
+
+  if ((req.url = '/')) {
+    fs.readFile(basePath, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.statusCode = 500
+        res.end()
+      } else {
+        res.write(data)
+        res.end()
+      }
+    })
+  } else {
+  }
+})
+
+server.listen(PORT, 'localhost', (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
